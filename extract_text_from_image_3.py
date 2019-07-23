198e62d046ad5a63da031df457035516668e6b6f
sherifi <sherifi_co@yahoo.com>
2019-07-23 14:16:58 +0200

    new file

---
+++ extract_text_from_image_3.py
@@ -0,0 +1,26 @@
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++
+# class Image():
+# 	#!/usr/bin/python
+# 	from os import listdir
+# 	from PIL import Image
+
+# 	def loadImages(path):
+# 		# return array of images
+
+# 		imagesList = listdir(path)
+# 		loadedImages = []
+# 		for image in imagesList:
+# 			img = Image.open(path + image)
+# 			loadedImages.append(img)
+
+# 		return loadedImages
+
+# 	path = "/path/to/your/images/"
+
+# 	# your images in an array
+# 	imgs = loadImages(path)
+
+# 	for img in imgs:
+# 		# you can show every image
+# 		img.show()
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++
